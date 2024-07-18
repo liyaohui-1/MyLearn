1affb1c2bba4f3d895f11c2d5597a744cf874977
liyaohui <yaohui.li@invo.cn>
2024-07-18 14:32:48 +0800

    feat:取长字节数据的某位

--- day25/main.c
+++ day25/main.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
+#include <stdint.h>
 
 int main(int argc,char* argv[])
 {
-    printf("hello world.\n");
+    uint32_t a = 8;
+    printf("a = %d.\n",(a &0x08) >> 3);
     return 0;
 }
