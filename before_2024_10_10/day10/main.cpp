9cf71284c078b636614275d014ff26c0b01ebab0
leesunl <leesunl@noreply.gitcode.com>
2024-10-16 11:51:08 +0800

    feat:add thread_pool_module image

--- before_2024_10_10/day10/main.cpp
+++ before_2024_10_10/day10/main.cpp
@@ -4,92 +4,82 @@
 #include <algorithm>
 
 //普通函数
-int commFunc(int lhs,int rhs)
-{
-    return lhs + rhs;
-}
+int commFunc(int lhs, int rhs) { return lhs + rhs; }
 
-//lambda函数表达式
-auto lamdaFunc = [] (int lhs,int rhs) -> int{return lhs + rhs;};
+// lambda函数表达式
+auto lamdaFunc = [](int lhs, int rhs) -> int { return lhs + rhs; };
 
 //函数对象
 class Functor
 {
-    public:
-        int operator()(int lhs,int rhs)
-        {
-            return lhs + rhs;
-        }
+public:
+    int operator()(int lhs, int rhs) { return lhs + rhs; }
 };
 
 //类成员函数
 class Randy
 {
-    public:
-        int randy_function(int lhs,int rhs){return lhs+rhs;}
-        static int static_randy_function(int lhs,int rhs){return lhs+rhs;}
-
+public:
+    int randy_function(int lhs, int rhs) { return lhs + rhs; }
+    static int static_randy_function(int lhs, int rhs) { return lhs + rhs; }
 };
 
-std::map<std::string,std::function<int(int,int)>> binOps = {
-    {"+",[](int a, int b)->int{return a+b;}},
-    {"-",[](int a, int b)->int{return a-b;}},
-    {"*",[](int a, int b)->int{return a*b;}},
-    {"/",[](int a, int b)->int{return a/b;}},
-    {"%",[](int a, int b)->int{return a%b;}},
+std::map<std::string, std::function<int(int, int)>> binOps = {
+    {"+", [](int a, int b) -> int {return a+b; }}, {"-", [](int a, int b) -> int {return a-b; }},
+    {"*", [](int a, int b) -> int {return a*b; }}, {"/", [](int a, int b) -> int {return a/b; }},
+    {"%", [](int a, int b) -> int {return a%b; }},
 };
 
 
 int main()
 {
-    std::function<int(int,int)> callback;
+    std::function<int(int, int)> callback;
     int result = -1;
 
     //普通函数
     callback = commFunc;
-    result = callback(1,2);
+    result = callback(1, 2);
     std::cout << "commFunc result: " << result << std::endl;
 
     //函数指针
     callback = &commFunc;
-    result = callback(1,2);
-    std::cout << "commFunc pointer: " << result <<std::endl;
+    result = callback(1, 2);
+    std::cout << "commFunc pointer: " << result << std::endl;
 
-    //lambda表达式
+    // lambda表达式
     callback = lamdaFunc;
-    result = lamdaFunc(1,2);
-    std::cout << "lamdaFunc : " << result <<std::endl;
+    result = lamdaFunc(1, 2);
+    std::cout << "lamdaFunc : " << result << std::endl;
 
     //函数对象
     Functor functor_randy;
     callback = functor_randy;
-    result = functor_randy(1,2);
-    std::cout << "class function : " << result <<std::endl;
+    result = functor_randy(1, 2);
+    std::cout << "class function : " << result << std::endl;
 
     //类成员函数(使用std::bind绑定类成员函数，std::placeholders::_1为占位符，顺序与原函数入参对应，顺序可调换)
-    //callback = std::bind(&Randy::randy_function,randy,std::placeholders::_2,std::placeholders::_1);
+    // callback = std::bind(&Randy::randy_function,randy,std::placeholders::_2,std::placeholders::_1);
     //上式也可以这么写，调用时，第2个参数在前，第1个参数在后
 
     // auto newcallback = std::bind(calllback, arg_list);
     //调用newcallback时，实际调用的是callback,并传给它arg_list的参数.
     Randy randy;
-    callback = std::bind(&Randy::randy_function,randy,std::placeholders::_1,std::placeholders::_2);
-    result = callback(1,2);
-    std::cout << "class member function : " << result <<std::endl;
+    callback = std::bind(&Randy::randy_function, randy, std::placeholders::_1, std::placeholders::_2);
+    result = callback(1, 2);
+    std::cout << "class member function : " << result << std::endl;
 
     //类静态成员
     callback = Randy::static_randy_function;
-    result = callback(1,2);
-    std::cout << "static class member function : " << result <<std::endl;
+    result = callback(1, 2);
+    std::cout << "static class member function : " << result << std::endl;
 
-    int left,right;
+    int left, right;
     std::string op;
-    while(true)
-    {
-        std::cin>>left>>op>>right;
-        std::cout <<left << op << right << "="<< binOps[op](left,right) << std::endl;
+    while (true) {
+        std::cin >> left >> op >> right;
+        std::cout << left << op << right << "=" << binOps[op](left, right) << std::endl;
     }
-    
+
 
     return 0;
 }
--- multi_thread_learn/thread/main.cpp
+++ multi_thread_learn/thread/main.cpp
@@ -1,61 +1,26 @@
 #include <iostream>
+#include <atomic>
 #include <thread>
-#include <mutex>
- 
-std::once_flag flag1;
- 
-void simple_do_once()
+
+namespace
 {
-    std::call_once(flag1, [](){ std::cout << "Simple example: called once\n"; });
+std::atomic<int> a;
 }
- 
+
+void fun()
+{
+    for (int i = 0; i < 100; ++i) {
+        a.fetch_add(1);
+    }
+}
+
 int main()
 {
-    std::thread st1(simple_do_once);
-    std::thread st2(simple_do_once);
-    std::thread st3(simple_do_once);
-    std::thread st4(simple_do_once);
-    st1.join();
-    st2.join();
-    st3.join();
-    st4.join();
+    std::thread t1(fun);
+    std::thread t2(fun);
+    t1.join();
+    t2.join();
+    std::cout << "value of a: " << a << std::endl;
+
+    return 0;
 }
-
-// #include <thread>
-// #include <string>
-// #include <iostream>
-
-// class A
-// {
-//     public:
-//         A() {}
-//         ~A() {}
-    
-//     void test(const std::string& str)
-//     {
-//         thread_local int count = 0;
-//         ++count;
-//         std::cout << str << " " << count << std::endl;
-//     }
-// };
-
-// void func(const std::string& str)
-// {
-//     A a1;
-//     a1.test(str);
-//     a1.test(str);
-
-//     A a2;
-//     a2.test(str);
-//     a2.test(str);
-// }
-
-// int main()
-// {
-//     std::thread t1(func, "t1");
-//     t1.join();
-//     std::thread t2(func, "t2");
-//     t2.join();
-
-//     return 0;
-// }
